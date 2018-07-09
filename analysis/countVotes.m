fdf1624abcf8791c877b5e1b9bfab344627a2deb
devins <devsullivan@gmail.com>
2018-07-09 14:02:29 +0200

    update to countVotes

--- analysis/countVotes.m
+++ analysis/countVotes.m
@@ -1,40 +1,46 @@
-function [u_classes,votes,votes_tot] = countVotes(votes,dictclasses,merge_level)
+function [u_classes,vote_nums,votes_tot,player_vote] = countVotes(votes,dictclasses,merge_level)
 
 if ~iscell(votes)
     votes = {votes};
 end
 
-currnums = cell(1,length(votes));
+numclasses = length(dictclasses);
+numplayers = length(votes);
 
+currnums = cell(1,numplayers);
+player_vote = zeros(numplayers,numclasses);
 for i = 1:length(votes)
     currvotes = votes{i};
     currnums{i} = cellfun(@str2num,strsplit(strrep(strrep(currvotes,'[',''),']',''),','));
+    currnums{i} = floor(currnums{i}.*10^-merge_level);
+    player_vote(i,:) = ismember(dictclasses,currnums{i});
 end
+
 
 allnums = cell2mat(currnums);
-allnums = floor(allnums.*10^-merge_level);
+% allnums = floor(allnums.*10^-merge_level);
 [u_classes,ia,ic] = unique(allnums);
+votes_tot = sum(player_vote);
 
-%force vector to ensure that each number is a bin. 
+
+% %force vector to ensure that each number is a bin. 
 u_classes = [0,u_classes];
-[votes] = hist(allnums,u_classes);
-peripheryind = u_classes==3;
-if merge_level==2 && sum(peripheryind)~=0
-    cytoind = u_classes==2;
-    votes(cytoind) = votes(cytoind)+votes(peripheryind);
-    votes(peripheryind) = [];
-    u_classes(peripheryind) = [];
-end
-if merge_level==2
-    dictclasses(dictclasses==3) = [];
-end
-
-
+[vote_nums] = hist(allnums,u_classes);
+% peripheryind = u_classes==3;
+% if merge_level==2 && sum(peripheryind)~=0
+%     cytoind = u_classes==2;
+%     vote_nums(cytoind) = vote_nums(cytoind)+vote_nums(peripheryind);
+%     vote_nums(peripheryind) = [];
+%     u_classes(peripheryind) = [];
+% end
+% if merge_level==2
+%     dictclasses(dictclasses==3) = [];
+% end
 
 u_classes(1) = [];
-votes(1) = [];
-votes_tot = zeros(size(dictclasses));
-for i = 1:length(u_classes)
-    currind = (u_classes(i)==dictclasses);
-    votes_tot(currind) = votes(i);
-end
+vote_nums(1) = [];
+% votes_tot = zeros(size(dictclasses));
+% for i = 1:length(u_classes)
+%     currind = (u_classes(i)==dictclasses);
+%     votes_tot(currind) = vote_nums(i);
+% end
