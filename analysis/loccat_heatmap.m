bf1b02b07be252978fd425b3bb734c5f127a7456
devins <devsullivan@gmail.com>
2018-07-09 14:07:10 +0200

    update to readers for this function

--- analysis/loccat_heatmap.m
+++ analysis/loccat_heatmap.m
@@ -1,5 +1,7 @@
-function loccat_heatmap(loccat_table_path)
+function loccat_heatmap(loccat_table_path,if_images_path)
 
 fid = fopen(loccat_table_path);
 
 
+dnn_data = xlsread('./binom_transfer_pseudo_gamer_v14.xls');
+dnn_numbers = csvread('./binom_transver_pseudo_gamer_v14_counts.txt',1);
